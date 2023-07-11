3c6529e896118955e786381d16b3b5016c32b66e
roronya <roronya628@gmail.com>
2023-07-11 12:30:41 +0900

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+window.onclick = () => {
+  document.querySelector("body > div.application.js-app.is-condensed > div.js-app-content.app-content > div.js-gryphon-beta-btn.gryphon-beta-btn-container").innerHTML=""
+};
